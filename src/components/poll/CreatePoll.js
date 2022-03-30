75841a6fb8282de3bdc485253613d5494f23e862
Hakim <me@hakim.ninja>
2022-03-30 17:35:04 +0200

    styling adding answer

--- src/components/poll/CreatePoll.js
+++ src/components/poll/CreatePoll.js
@@ -1,8 +1,18 @@
 import React, { useRef } from 'react'
-import {InputAdornment, IconButton, Box, TextField, Typography, Paper} from '@mui/material';
+import {
+  InputAdornment,
+  IconButton,
+  Box,
+  TextField,
+  Typography,
+  Paper,
+  Chip,
+  Button
+} from '@mui/material';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { display } from '@mui/system';
 export default function CreatePoll(props) {
   const {
     onQuestionChange,
@@ -21,77 +31,77 @@ export default function CreatePoll(props) {
 
   return (
     <>
-      <div>
-        <TextField
-          label="Question"
-          value={state.question}
-          onChange={onQuestionChange}
-          fullWidth
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <QuestionMarkIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Box mt={2}>
-          <Typography> Answers</Typography>
+      <TextField
+        label="Question"
+        value={state.question}
+        onChange={onQuestionChange}
+        fullWidth
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <QuestionMarkIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Box mt={2}>
+        <Typography> Answers</Typography>
         {Object.keys(state.options).map((id) => (
-            <Box key={id} mt={2}>
-              <TextField
-                value={state.options[id].text}
-                onChange={(e) => onOptionChange(e, id)}
-                fullWidth
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={() => { onDeleteOption(id) }}
-                        onMouseDown={() => { onDeleteOption(id) }}
-                        edge="end"
-                      >
-                       <DeleteOutlineIcon />
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Box>
-          ))
-        }
-        </Box>
-        {Object.keys(state.options).length < 10 &&
-          <Paper elevation={8}>
-            <Box p={2} mt={2}>
+          <Box key={id} mt={2}>
             <TextField
-                fullWidth
-                label="Add Option"
-                inputRef={OptionRef}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={passOption}
-                        onMouseDown={passOption}
-                        edge="end"
-                      >
-                       <AddCircleOutlineIcon />
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
+              value={state.options[id].text}
+              onChange={(e) => onOptionChange(e, id)}
+              fullWidth
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => { onDeleteOption(id) }}
+                      onMouseDown={() => { onDeleteOption(id) }}
+                      edge="end"
+                    >
+                      <DeleteOutlineIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+        ))
+        }
+      </Box>
+      {Object.keys(state.options).length < 10 &&
+        <Paper elevation={8}>
+          <Box p={2} mt={2}>
+            <TextField
+              fullWidth
+              label="Add Option"
+              inputRef={OptionRef}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={passOption}
+                      onMouseDown={passOption}
+                      edge="end"
+                    >
+                      <AddCircleOutlineIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
             {/* <input ref={OptionRef} type="text" />
             <button onClick={passOption}>Add</button> */}
-            </Box>
-          </Paper>
-        }
-        <p>{Object.keys(state.options).length}/10 possible Answers</p>
-      </div>
-      <button onClick={onResetData}>Reset</button>
+          </Box>
+        </Paper>
+      }
+      <Box mt={3} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Chip label={`${Object.keys(state.options).length}/10 possible Answers`} />
+        <Button variant='outlined' onClick={onResetData}>Reset</Button>
+      </Box>
     </>
   )
 }

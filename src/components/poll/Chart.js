d4b16a93238a9a65211dcf0f9e5898de3bc5fc08
Hakim <me@hakim.ninja>
2022-03-31 13:08:11 +0200

    styling chart

--- src/components/poll/Chart.js
+++ src/components/poll/Chart.js
@@ -2,28 +2,41 @@ import React from 'react'
 import {
   Box,
   Chip,
-  Button
+  Typography
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-const Item = styled(Box)(({ theme }) => ({
-  backgroundColor: '#eee',
-  // padding: theme.spacing(2),
-  color: theme.palette.text.secondary,
-  height: 300,
-  width: '100%',
-  position: 'relative',
-  display:'flex',
-  justifyContent:'center',
-  paddingTop:10,
+const Item = styled(Box)(() => ({
+  height: 350,
+  width: 55,
+  display: 'flex',
+  paddingTop: 10,
+  flexDirection: 'column',
 }));
-const Bar = styled(Box)(({ theme }) => ({
-  backgroundColor: '#f00',
-  color: theme.palette.text.secondary,
+const BarWraper = styled(Box)(() => ({
+  backgroundColor: '#eee',
+  position: 'relative',
+  flexDirection: 'row',
+  display: 'flex'
+}));
+const Bar = styled(Box)(() => ({
   height: '50%',
-  width: '100%',
+  width: 30,
   bottom: 0,
-  position: "absolute"
+  left: 25,
+  position: "absolute",
+  zIndex: 1
+
+}));
+const OptionText = styled(Box)(() => ({
+  whiteSpace: 'nowrap',
+  transform: 'rotate(-90deg)',
+  transformOrigin: '0 0',
+  bottom: -23,
+  left: 0,
+  position: "absolute",
+  letterSpacing: '2px',
+  textTransform: 'uppercase'
 
 }));
 
@@ -32,26 +45,20 @@ export default function Vote(props) {
   const percentage = (partialValue) => (100 * partialValue) / state.totalVotes
   return (
     <Box >
+      <Typography sx={{ marginBottom: '10px' }} variant='h4'>{state.question}</Typography>
       <Chip label={` Total Votes = ${state.totalVotes}`} />
       <Box mt={3} sx={{ display: 'flex', gap: "10px" }}>
         {Object.keys(state.options).map((id) => (
-          <Item>
-            <Chip label={state.options[id].votes} />
-            <Bar sx={{ height: `${percentage(state.options[id].votes).toFixed(2)}%`, background: state.options[id].color }} ></Bar>
+          <Item key={id}>
+            <Box display="flex" justifyContent="center"><Chip label={state.options[id].votes} /></Box>
+            <BarWraper my={2} display="flex" flexGrow={1}>
+              <Box><OptionText flexGrow={0} display="flex" justifyContent="center">{state.options[id].text}</OptionText></Box>
+              <Box><Bar sx={{ height: `${percentage(state.options[id].votes).toFixed(2)}%`, background: state.options[id].color }} /></Box>
+            </BarWraper>
           </Item>
         ))}
-
-        {/* <ul>
-        {Object.keys(state.options).map((id) => (
-          <li key={id}>
-            {state.options[id].text} - {state.options[id].votes} - {percentage(state.options[id].votes).toFixed(2)}
-          </li>
-
-        ))}
-      </ul> */}
-
       </Box>
-      
+
     </Box>
   )
 }
